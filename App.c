9369c9d8bb3f8e705e9638d732d2fcbaa8272801
Man3om <menem.magdi222@gmail.com>
2023-06-15 23:48:46 +0300

    First Comm

---
+++ App.c
@@ -0,0 +1,98 @@
+ /******************************************************************************
+ *
+ * Layer: Application Layer
+ *
+ * File Name: APP.c
+ *
+ * Description: Source file for the Application Layer
+ *
+ * Author: Abdelmonem Magdi
+ *
+ *******************************************************************************/
+
+#include "keypad.h"
+#include "lcd.h"
+#include <util/delay.h>
+
+/*
+ * Description :
+ * Calculate The Result Between 2 Numbers
+ * Add / Sub / Divide / MUL
+ */
+
+uint32 calc(uint32 v1 , uint32 v2 , uint32 c)
+{
+	uint32 Res = 0 ;
+
+	switch(c)
+	{
+	case '+':
+		Res = v1 + v2 ;
+		break ;
+	case '-':
+		Res = v1 - v2 ;
+		break;
+	case '/':
+		Res = v1 / v2 ;
+		break;
+	case '*':
+		Res = v1 * v2 ;
+		break;
+	default:
+		break;
+	}
+
+	return Res;
+}
+
+/* This Code is Written for solve Simple Calculation between 2 numbers only*/
+
+int main(void)
+{
+	uint8 key = 0 , op = '+' , flag = 0 ; /* Using Flag To Control Which Var should be stored in*/ 
+	uint32 var1 = 0 , var2 = 0 ;
+
+	LCD_init() ; /* Initialization LCD */
+
+	while(1)
+	{
+		key = KEYPAD_getPressedKey();
+
+		if(key == '=') // Equal and Calculate The Result
+		{
+			LCD_moveCursor(1,10); // move the crusor to Row 1 and column 10
+			LCD_intgerToString(calc(var1,var2,op));
+			flag = 0 ;
+			var1 = 0 ;
+			var2 = 0 ;
+		}
+		else if(key == 13) // on/off Button to (Clear The LCD)
+		{
+			LCD_clearScreen(); // Clear Screen
+			var1 = 0 ;
+			flag = 0 ;
+			var2 = 0 ;
+		}
+		else if((key >= 0) && (key <= 9)) // Store The 2 Numbers 
+		{
+			if(flag == 0)
+			{
+				var1 = (var1 * 10) + key ;
+				LCD_intgerToString(key);
+			}
+			else
+			{
+				var2 = (var2 * 10) + key ;
+				LCD_intgerToString(key);
+			}
+		}
+		else /* Enter here if The operator's Button was pressed */
+		{
+			op = key ;
+			LCD_displayCharacter(key);
+			flag = 1 ;
+		}
+
+		_delay_ms(500);
+	}
+}
---
+++ Debug/App.d
@@ -0,0 +1,7 @@
+App.o App.o: ../App.c ../keypad.h ../std_types.h ../lcd.h
+
+../keypad.h:
+
+../std_types.h:
+
+../lcd.h:
